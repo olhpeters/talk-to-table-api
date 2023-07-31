8ddf94fdac9a3e9d959c1ba752666b8e27be638b
Oliver Peters <olhpeters@gmail.com>
2023-07-30 20:49:19 -0400

    added sql query to duckdb

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import traceback
 from pydantic import BaseModel
-from ttt.chat import chat
+from ttt.chat import table_chat, error_chat
+from ttt.utils import convert_scientific_to_number
+import uuid
+import duckdb
+import json
 
 
 app = FastAPI()
@@ -18,52 +22,98 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+tables = {}
+
+
 class ChatRequest(BaseModel):
     talk_input: str
+    session: str
+
 
 @app.get("/hello")
 async def root():
     return {"message": "Hello World"}
-        
+
+
 @app.post("/upload")
 def upload(file: UploadFile = File(...)):
-
     print("POST /upload")
     tabledata = [
-        {"id":1, "name":"Oliver", "age":"12", "col":"red", "dob":""},
-        {"id":2, "name":"Mary May", "age":"1", "col":"blue", "dob":"14/05/1982"},
-        {"id":3, "name":"Christine Lobowski", "age":"42", "col":"green", "dob":"22/05/1982"},
-        {"id":4, "name":"Brendon Philips", "age":"125", "col":"orange", "dob":"01/08/1980"},
-        {"id":5, "name":"Margret Marmajuke", "age":"16", "col":"yellow", "dob":"31/01/1999"},
+        {"id": 1, "name": "Oliver", "age": "12", "col": "red", "dob": ""},
+        {"id": 2, "name": "Mary May", "age": "1", "col": "blue", "dob": "14/05/1982"},
+        {
+            "id": 3,
+            "name": "Christine Lobowski",
+            "age": "42",
+            "col": "green",
+            "dob": "22/05/1982",
+        },
+        {
+            "id": 4,
+            "name": "Brendon Philips",
+            "age": "125",
+            "col": "orange",
+            "dob": "01/08/1980",
+        },
+        {
+            "id": 5,
+            "name": "Margret Marmajuke",
+            "age": "16",
+            "col": "yellow",
+            "dob": "31/01/1999",
+        },
     ]
+
+    session = None
 
     try:
         # with open(file.filename, 'wb') as f:
-            # while contents := file.file.read(1024 * 1024):
-            #     f.write(contents)
+        # while contents := file.file.read(1024 * 1024):
+        #     f.write(contents)
         table = pd.read_csv(file.file)
-        tabledata = table.to_json(orient='records')
+        table.columns = table.columns.str.replace(" ", "_")
+        table = table.applymap(convert_scientific_to_number)
+        session = str(uuid.uuid4())
+        tables[session] = table
+        tabledata = table.to_json(orient="records")
     except Exception:
         traceback.print_exc()
-        return {"message": "There was an error uploading the file"}
+        raise HTTPException(
+            status_code=500, detail="There was a problem uploading your file"
+        )
     finally:
         file.file.close()
 
     print(tabledata)
     return {
-        "message": f"Successfully uploaded {file.filename}", 
-        "table": tabledata
+        "message": f"Successfully uploaded {file.filename}",
+        "table": tabledata,
+        "session": session,
     }
+
 
 @app.post("/chat")
 def do_chat(chat_request: ChatRequest):
     print("POST /chat")
 
-    print(chat_request.talk_input)
+    print(chat_request)
 
-    response = chat(chat_request.talk_input)
+    try:
+        current_df = tables.get(chat_request.session)
 
-    return {
-        "message": response, 
-    }
+        response = table_chat(chat_request.talk_input, current_df)
+        response_json = json.loads(response)
 
+        print(response_json)
+
+        if response_json.get("sql"):
+            response_df = duckdb.query(response_json.get("sql")).df()
+    except Exception:
+        traceback.print_exc()
+        exception_summary = traceback.format_exc()
+        text_response = error_chat(exception_summary)
+        raise HTTPException(
+            status_code=500, detail=text_response
+        )
+
+    return {"message": response, "table": response_df.to_json(orient="records")}
--- ttt/chat.py
+++ ttt/chat.py
@@ -7,8 +7,26 @@ dotenv_file = dotenv.find_dotenv()
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def chat(prompt): 
+def table_chat(prompt, current_df): 
     print(f"chatting using prompt: {prompt}")
-    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])
+
+    first_three_rows = current_df.head(3).to_json(orient='records')
+    colstr = ""
+    for col in current_df.columns:
+        colstr += f' "{col}" (type: {current_df[col].dtype}),' 
+
+    system_prompt = "Act as a code library that returns SQL code for an arbitrary natural language statement made against a data table named 'current_df', Here are the column names of the table and their data format : {colstr}. The first three rows have the following values : {first_three_rows}. Return your response in json format. The json will have three fields. The first is 'action', the second is 'sql' and the third is 'message'. The 'action' field will be a static value, either 'SQL_QUERY', 'SQL_UPDATE', or 'MESSAGE'. The 'sql' field will contain a SQL statement to run on the table to resolve the user request (or empty if the response is a message). The SQL can result in either a single value to return to the user (i.e; a count) which is a SQL_QUERY action, or a new view of the data table which is a SQL_UPDATE action. If you are unable to understand the user request or need further clarification, use the 'message' field and action to return a conversational text message in response to the user making the request. IMPORTANT: When performing an update, instead of providing an ALTER TABLE command, return a SQL SELECT statement that provides a new view of the data."
+
+    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": prompt}
+        ])
     print(chat_completion)
     return chat_completion.choices[0].message.content
+
+def error_chat(exception_summary):
+   prompt = f'We recieved an exception from running the code : {exception_summary}.  Please put this error in a human readable form.'
+   chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
+            {"role": "user", "content": prompt}
+        ])
+   return chat_completion.choices[0].message.content
